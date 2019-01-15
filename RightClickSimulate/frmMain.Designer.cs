a5b33a9e41a025d35f3adb5ad626012c201c6742
Furkan EKİNCİ <furkanekinci@hotmail.com>
2019-01-15 09:39:52 +0300

    First commit.
    Repository created.

---
+++ RightClickSimulate/frmMain.Designer.cs
@@ -0,0 +1,111 @@
+﻿namespace RightClickSimulate
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmMain));
+            this.doubleClickTimer = new System.Windows.Forms.Timer(this.components);
+            this.sysTray = new System.Windows.Forms.NotifyIcon(this.components);
+            this.cntMnuSysTray = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.cntMnuSysTray_Static = new System.Windows.Forms.ToolStripMenuItem();
+            this.cntMnuSysTray_Exit = new System.Windows.Forms.ToolStripMenuItem();
+            this.cntMnuSysTray.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // sysTray
+            // 
+            this.sysTray.ContextMenuStrip = this.cntMnuSysTray;
+            this.sysTray.Icon = ((System.Drawing.Icon)(resources.GetObject("sysTray.Icon")));
+            this.sysTray.Text = "Right Click Simulate";
+            this.sysTray.Visible = true;
+            this.sysTray.MouseClick += new System.Windows.Forms.MouseEventHandler(this.sysTray_MouseClick);
+            this.sysTray.MouseUp += new System.Windows.Forms.MouseEventHandler(this.sysTray_MouseUp);
+            // 
+            // cntMnuSysTray
+            // 
+            this.cntMnuSysTray.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.cntMnuSysTray_Static,
+            this.cntMnuSysTray_Exit});
+            this.cntMnuSysTray.Name = "cntMnuSysTray";
+            this.cntMnuSysTray.Size = new System.Drawing.Size(101, 48);
+            this.cntMnuSysTray.MouseLeave += new System.EventHandler(this.cntMnuSysTray_MouseLeave);
+            // 
+            // cntMnuSysTray_Static
+            // 
+            this.cntMnuSysTray_Static.CheckOnClick = true;
+            this.cntMnuSysTray_Static.Name = "cntMnuSysTray_Static";
+            this.cntMnuSysTray_Static.Size = new System.Drawing.Size(100, 22);
+            this.cntMnuSysTray_Static.Text = "Sabit";
+            this.cntMnuSysTray_Static.CheckedChanged += new System.EventHandler(this.cntMnuSysTray_Static_CheckedChanged);
+            // 
+            // cntMnuSysTray_Exit
+            // 
+            this.cntMnuSysTray_Exit.Name = "cntMnuSysTray_Exit";
+            this.cntMnuSysTray_Exit.Size = new System.Drawing.Size(100, 22);
+            this.cntMnuSysTray_Exit.Text = "Çıkış";
+            this.cntMnuSysTray_Exit.Click += new System.EventHandler(this.cntMnuSysTray_Exit_Click);
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(50, 50);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.KeyPreview = true;
+            this.MinimumSize = new System.Drawing.Size(50, 50);
+            this.Name = "frmMain";
+            this.ShowInTaskbar = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Right Click Simulate";
+            this.TopMost = true;
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.frmMain_FormClosed);
+            this.Load += new System.EventHandler(this.frmMain_Load);
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.frmMain_Paint);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.frmMain_KeyDown);
+            this.MouseClick += new System.Windows.Forms.MouseEventHandler(this.frmMain_MouseClick);
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.frmMain_MouseDown);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.frmMain_MouseMove);
+            this.MouseUp += new System.Windows.Forms.MouseEventHandler(this.frmMain_MouseUp);
+            this.cntMnuSysTray.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer doubleClickTimer;
+        private System.Windows.Forms.NotifyIcon sysTray;
+        private System.Windows.Forms.ContextMenuStrip cntMnuSysTray;
+        private System.Windows.Forms.ToolStripMenuItem cntMnuSysTray_Static;
+        private System.Windows.Forms.ToolStripMenuItem cntMnuSysTray_Exit;
+
+    }
+}
+
---
+++ RightClickSimulate/frmMain.cs
@@ -0,0 +1,216 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Windows.Forms;
+
+namespace RightClickSimulate
+{
+    public partial class frmMain : Form
+    {
+        bool isStatic = false;
+
+        MouseButtons clickedButton = MouseButtons.None;
+        Point firstMouseDown = new Point();
+        Point firstFormPos = new Point();
+
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        [Flags]
+        public enum MouseEventFlags
+        {
+            LEFTDOWN = 0x00000002,
+            LEFTUP = 0x00000004,
+            MIDDLEDOWN = 0x00000020,
+            MIDDLEUP = 0x00000040,
+            MOVE = 0x00000001,
+            ABSOLUTE = 0x00008000,
+            RIGHTDOWN = 0x00000008,
+            RIGHTUP = 0x00000010
+        }
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, CallingConvention = CallingConvention.StdCall)]
+        public static extern void mouse_event(long dwFlags, long dx, long dy, long cButtons, long dwExtraInfo);
+
+        public void DoMouseClick(MouseEventFlags firstEvent, MouseEventFlags secondEvent, Point position)
+        {
+            Point curPos = Cursor.Position;
+            int X = position.X;
+            int Y = position.Y;
+            Cursor.Position = new Point(X - 1, Y - 1);
+            mouse_event((int)firstEvent | (int)secondEvent, X, Y, 0, 0);
+        }
+
+        private Color GetReadableForeColor(Color c)
+        {
+            return Color.FromArgb(c.R ^ 0x80, c.G ^ 0x80, c.B ^ 0x80);
+        }
+
+        private void frmMain_MouseDown(object sender, MouseEventArgs e)
+        {
+            clickedButton = e.Button;
+            firstMouseDown = Cursor.Position;
+            firstFormPos = this.Location;
+        }
+        private void frmMain_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (clickedButton != MouseButtons.None)
+            {
+                if (Cursor.Position.X > firstMouseDown.X)
+                {
+                    this.Left = firstFormPos.X + (Cursor.Position.X - firstMouseDown.X);
+                }
+                else if (Cursor.Position.X < firstMouseDown.X)
+                {
+                    this.Left = firstFormPos.X - (firstMouseDown.X - Cursor.Position.X);
+                }
+
+                if (Cursor.Position.Y > firstMouseDown.Y)
+                {
+                    this.Top = firstFormPos.Y + (Cursor.Position.Y - firstMouseDown.Y);
+                }
+                else if (Cursor.Position.Y < firstMouseDown.Y)
+                {
+                    this.Top = firstFormPos.Y - (firstMouseDown.Y - Cursor.Position.Y);
+                }
+            }
+        }
+        private void frmMain_MouseUp(object sender, MouseEventArgs e)
+        {
+            //if (ModifierKeys != Keys.Control)
+            //{
+            //    switch (clickedButton)
+            //    {
+            //        case MouseButtons.Left:
+            //            DoMouseClick(MouseEventFlags.LEFTDOWN, MouseEventFlags.LEFTUP, this.Location);
+            //            break;
+            //        case MouseButtons.Middle:
+            //            DoMouseClick(MouseEventFlags.MIDDLEDOWN, MouseEventFlags.MIDDLEUP, this.Location);
+            //            break;
+            //        case MouseButtons.Right:
+            //            DoMouseClick(MouseEventFlags.RIGHTDOWN, MouseEventFlags.RIGHTUP, this.Location);
+            //            break;
+            //    }
+            //}
+
+            if (clickedButton == MouseButtons.Left)
+            {
+                DoMouseClick(MouseEventFlags.RIGHTDOWN, MouseEventFlags.RIGHTUP, this.Location);
+            }
+
+            clickedButton = MouseButtons.None;
+
+            if (isStatic)
+                this.Location = firstFormPos;
+        }
+
+        private void frmMain_Load(object sender, EventArgs e)
+        {
+            sysTray.Visible = true;
+        }
+        private void frmMain_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Escape)
+            {
+                //clickedButton = MouseButtons.None;
+
+                //if (MessageBox.Show(this, "Are you sure want to exit?", "Exit?", MessageBoxButtons.YesNo) == System.Windows.Forms.DialogResult.Yes)
+                //{
+                Application.Exit();
+                //}
+            }
+        }
+        private void frmMain_Paint(object sender, PaintEventArgs e)
+        {
+            Bitmap bmp = new Bitmap(250, 200);
+            Graphics g = this.CreateGraphics();
+            g = Graphics.FromImage(bmp);
+            g.CopyFromScreen(this.Location.X - 20, this.Location.Y - 20, 10, 10, new Size(100, 100));
+
+            Bitmap bmpZoom = bmp;
+            g = Graphics.FromImage(bmpZoom);
+
+            int new4W = Convert.ToInt32(bmp.Width / 2.5);
+            int new4H = Convert.ToInt32(bmp.Height / 2.5);
+            int new2W = bmp.Width / 2;
+            int new2H = bmp.Height / 2;
+
+            Rectangle srcRect = new Rectangle(new4W - 88, new4H - 68, new2W, new2H);
+            Rectangle dstRect = new Rectangle(0, 0, bmpZoom.Width, bmpZoom.Height);
+            g.DrawImage(bmp, dstRect, srcRect, GraphicsUnit.Pixel);
+
+            this.BackgroundImage = bmp;
+
+            GraphicsPath myPath = new GraphicsPath();
+            GraphicsPath myPath2 = new GraphicsPath();
+
+            Point[] myArray2 =
+             {
+                 new Point(20,40),
+                 new Point(0,0),
+                 new Point(40,20),
+                 new Point(40,40)
+             };
+
+            myPath2.AddLines(myArray2);
+            myPath.AddPath(myPath2, true);
+
+            this.Region = new Region(myPath);
+
+            e.Graphics.DrawRectangle(new Pen(GetReadableForeColor((this.BackgroundImage as Bitmap).GetPixel(10, 10)), 20), this.DisplayRectangle);
+        }
+
+        private void frmMain_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (cntMnuSysTray.Visible)
+            {
+                cntMnuSysTray.Close();
+            }
+        }
+        private void frmMain_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            sysTray.Visible = false;
+        }
+
+        private void cntMnuSysTray_Static_CheckedChanged(object sender, EventArgs e)
+        {
+            isStatic = cntMnuSysTray_Static.Checked;
+        }
+        private void cntMnuSysTray_Exit_Click(object sender, EventArgs e)
+        {
+            //if (MessageBox.Show(this, "Are you sure want to exit?", "Exit?", MessageBoxButtons.YesNo) == System.Windows.Forms.DialogResult.Yes)
+            //{
+            Application.Exit();
+            //}
+        }
+
+        private void cntMnuSysTray_MouseLeave(object sender, EventArgs e)
+        {
+            cntMnuSysTray.Close();
+        }
+
+        private void sysTray_MouseClick(object sender, MouseEventArgs e)
+        {
+            cntMnuSysTray.Show(Control.MousePosition);
+        }
+        private void sysTray_MouseUp(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                MethodInfo mi = typeof(NotifyIcon).GetMethod("ShowContextMenu", BindingFlags.Instance | BindingFlags.NonPublic);
+                mi.Invoke(sysTray, null);
+            }
+        }
+
+    }
+}
